3b3607b505b0d4f1ccc68adcc87cf424adda3e02
Lumina <alexander.andrejeff@elev.ga.lbs.se>
2023-01-20 11:26:26 +0100

    *
    Add Configurable gun parameters

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bulletScript : MonoBehaviour
+public class BulletScript : MonoBehaviour
 {
     Rigidbody2D bulletRigidbody2D;
     Collider2D collider2D;
@@ -14,24 +14,18 @@ public class bulletScript : MonoBehaviour
     void Start()
     {
         bulletRigidbody2D = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<Collider2D>();
+        collider2D        = GetComponent<Collider2D>();
     }
 
-    void Update()
-    {
-        bulletRigidbody2D.AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse);
-    }
+    void Update() { bulletRigidbody2D.AddForce(transform.right * bulletSpeed, ForceMode2D.Impulse); }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (collider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
-        {
             bulletRigidbody2D.constraints = RigidbodyConstraints2D.FreezeAll;
-        }
-        if (collider2D.IsTouchingLayers(LayerMask.GetMask("Enemy")))
-        {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
-        }
+
+        if (!collider2D.IsTouchingLayers(LayerMask.GetMask("Enemy"))) return;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,33 +3,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class Gun : MonoBehaviour
 {
-    [Header("Configurable Parameters")]
-
-    [SerializeField] GameObject bullet;
-    [FormerlySerializedAs("projectileScript")]
-    [SerializeField] bulletScript bulletScript;
-    [SerializeField] PlayerMovement playerMovement;
-
-    void Start()
+    public enum FireMode
     {
+        Semi,
+        Shotgun
+    }
+
+    FireMode currentFireMode;
+
+    [Header("Configurable Parameters")]
+    [SerializeField] GameObject bullet;
+    [SerializeField] BulletScript bulletScript;
+    [SerializeField] float lifeTime;
+
+    [SerializeField] int pelletCount;
+    [SerializeField] float spreadAngle;
+    [SerializeField] float pelletFireVel;
+    [SerializeField] GameObject pellet;
+    [SerializeField] Transform barrelExit;
+    List<Quaternion> pellets;
+
+    [SerializeField] bool isSemi_Automatic;
+    [SerializeField] bool isShotgun;
+
+    void Awake()
+    {
+        pellets = new List<Quaternion>(pelletCount);
+
+        for (int i = 0; i < pelletCount; i++) { pellets.Add(Quaternion.Euler(Vector3.zero)); }
     }
 
     void Update()
     {
-        Shoot(bulletType: bullet, bulletSpeed: bulletScript.bulletSpeed, lifeTime: bulletScript.bulletLifeTime);
+        if (Input.GetKeyDown(KeyCode.Mouse0)) Shoot(currentFireMode);
     }
 
-    public void Shoot(GameObject bulletType, float bulletSpeed, float lifeTime)
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            Instantiate(bulletType, new Vector3(transform.position.x + 1, transform.position.y), transform.rotation);
-            bulletType.transform.Translate(transform.right * (bulletSpeed * Time.deltaTime));
+    #region MyRegion
+    // void AlternateShoot()
+    // {
+    //     int i=0;
+    //     foreach (Quaternion quat in pellets)
+    //     {
+    //         pellets[i] = Random.rotation;
+    //         GameObject p = Instantiate(pellet, barrelExit.position, barrelExit.rotation);
+    //         p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+    //         p.GetComponent<Rigidbody2D>().AddForce(p.transform.right * pelletFireVel);
+    //         i++;
+    //     }
+    // }
+    #endregion
 
-            //Destroy(bulletType, lifeTime);
+    void Shoot(Enum fireMode)
+    {
+        switch (fireMode)
+        {
+            case FireMode.Semi:
+                GameObject b = Instantiate(bullet, barrelExit.position, barrelExit.rotation);
+                b.GetComponent<Rigidbody2D>().AddForce(b.transform.right * bulletScript.bulletSpeed);
+                Destroy(b, lifeTime);
+                break;
+
+            case FireMode.Shotgun:
+                int i = 0;
+
+                foreach (Quaternion quat in pellets)
+                {
+                    pellets[i] = Random.rotation;
+                    GameObject p = Instantiate(pellet, barrelExit.position, barrelExit.rotation);
+                    p.transform.rotation = Quaternion.RotateTowards(p.transform.rotation, pellets[i], spreadAngle);
+                    p.GetComponent<Rigidbody2D>().AddForce(p.transform.right * pelletFireVel);
+                    i++;
+                }
+
+                break;
         }
     }
 }
